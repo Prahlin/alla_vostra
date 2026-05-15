2a79fed8a32975cb6c0de2824266a2a9960ebe04
Prahlin <martin@prahlproductions.com>
2026-05-15 16:39:33 -0400

    created completely unique header portion (orange banner, white top nav bar & top banner image) to be used only during 'thin' horizontal widths of the web version, as well as a soon-to-be mobile app version. marquee scrolling text has been removed from the orange banner, and the orange banner increased in height. alla vostra text logo has been relocated to the orange bar, increased in font size and changed to white font color in order to stand out over the orange banner. 'Shop' button has also been increased in size, had its colors inverted, and placed inside the orange banner with a bold font. The white top nav bar has been increased in height, with font size drastically enlarged for improved visibility during 'profile' aspect ratios. In addition, only one link ('products', 'about us', 'contact') now appears inside the white nav bar, centered. Horizontal mouse click-and-drag functionality implemented for the user, allowing them to click and drag the cursor to flip between pages/screens. Outward-pointing, "pulsating" arrows added around each link (page) name, alternating between low and high opacity to indicate "carousel" functionality in place for an enhanced user experience. Top banner image substantially increased in size to mirror all of the asset size increases on the top of the screen.

--- other/assets/js/header.js
+++ other/assets/js/header.js
@@ -4,12 +4,15 @@ if (siteHeaderMount) {
   siteHeaderMount.innerHTML = `
     <header>
       <div class="announcement">
-        <span class="announcement-text">
-          Order the Alla Vostra favorites in time for the holidays
-        </span>
+        <a class="mobile-orange-logo" href="index.html">
+          Alla<br />
+          Vostra
+        </a>
+
+        <a class="mobile-orange-shop" href="shop.html">SHOP</a>
       </div>
 
-      <nav class="nav">
+      <nav class="nav nav-desktop">
         <a class="logo" href="index.html">
           Alla<br />
           Vostra
@@ -19,6 +22,33 @@ if (siteHeaderMount) {
         <a class="nav-link" href="products.html">Products</a>
         <a class="nav-link" href="aboutus.html">About Us</a>
         <a class="nav-link" href="contact.html">Contact</a>
+      </nav>
+
+      <nav class="nav-mobile">
+        <div class="mobile-top-row">
+          <a class="mobile-logo" href="index.html">
+            Alla<br />
+            Vostra
+          </a>
+
+          <a class="mobile-shop" href="shop.html">SHOP</a>
+        </div>
+
+        <div class="mobile-bottom-row">
+          <div class="mobile-carousel-window">
+            <div class="mobile-carousel-track">
+              <a class="mobile-carousel-link" href="products.html" data-mobile-nav-index="0">
+                Products
+              </a>
+              <a class="mobile-carousel-link" href="aboutus.html" data-mobile-nav-index="1">
+                About Us
+              </a>
+              <a class="mobile-carousel-link" href="contact.html" data-mobile-nav-index="2">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
 
@@ -58,3 +88,196 @@ window.addEventListener("scroll", updateHeroScale, { passive: true });
 window.addEventListener("resize", updateHeroScale);
 
 updateHeroScale();
+
+const mobileNavLinks = Array.from(document.querySelectorAll(".mobile-carousel-link"));
+const mobileCarouselWindow = document.querySelector(".mobile-carousel-window");
+const mobileCarouselTrack = document.querySelector(".mobile-carousel-track");
+
+let mobileNavIndex = 0;
+let mobileDragStartX = 0;
+let mobileDragCurrentX = 0;
+let mobileDragging = false;
+
+function getMobileNavIndexFromPath() {
+  const path = window.location.pathname.split("/").pop();
+
+  if (path === "aboutus.html") {
+    return 1;
+  }
+
+  if (path === "contact.html") {
+    return 2;
+  }
+
+  return 0;
+}
+
+function setMobileDragOffset(value) {
+  if (!mobileCarouselTrack) {
+    return;
+  }
+
+  mobileCarouselTrack.style.setProperty("--mobile-drag-offset", `${value}px`);
+}
+
+function renderMobileNav() {
+  if (!mobileNavLinks.length) {
+    return;
+  }
+
+  mobileNavLinks.forEach((link, index) => {
+    link.classList.remove("is-left", "is-center", "is-right", "is-hidden");
+
+    if (index === mobileNavIndex) {
+      link.classList.add("is-center");
+      return;
+    }
+
+    if (index === (mobileNavIndex + mobileNavLinks.length - 1) % mobileNavLinks.length) {
+      link.classList.add("is-left");
+      return;
+    }
+
+    if (index === (mobileNavIndex + 1) % mobileNavLinks.length) {
+      link.classList.add("is-right");
+      return;
+    }
+
+    link.classList.add("is-hidden");
+  });
+}
+
+function moveMobileNav(direction) {
+  if (direction === "left") {
+    mobileNavIndex = (mobileNavIndex + 1) % mobileNavLinks.length;
+  }
+
+  if (direction === "right") {
+    mobileNavIndex =
+      (mobileNavIndex + mobileNavLinks.length - 1) % mobileNavLinks.length;
+  }
+
+  setMobileDragOffset(0);
+  renderMobileNav();
+}
+
+if (mobileCarouselWindow && mobileNavLinks.length) {
+  mobileNavIndex = getMobileNavIndexFromPath();
+  renderMobileNav();
+
+  mobileCarouselWindow.addEventListener("mousedown", (event) => {
+    mobileDragging = true;
+    mobileDragStartX = event.clientX;
+    mobileDragCurrentX = event.clientX;
+    mobileCarouselWindow.classList.add("is-dragging");
+  });
+
+  window.addEventListener("mousemove", (event) => {
+    if (!mobileDragging) {
+      return;
+    }
+
+    mobileDragCurrentX = event.clientX;
+    const dragDistance = mobileDragCurrentX - mobileDragStartX;
+
+    setMobileDragOffset(dragDistance);
+  });
+
+  window.addEventListener("mouseup", () => {
+    if (!mobileDragging) {
+      return;
+    }
+
+    const dragDistance = mobileDragCurrentX - mobileDragStartX;
+    const dragThreshold = 32;
+
+    mobileCarouselWindow.classList.remove("is-dragging");
+
+    if (dragDistance < -dragThreshold) {
+      moveMobileNav("left");
+      mobileDragging = false;
+      return;
+    }
+
+    if (dragDistance > dragThreshold) {
+      moveMobileNav("right");
+      mobileDragging = false;
+      return;
+    }
+
+    setMobileDragOffset(0);
+    mobileDragging = false;
+  });
+
+  mobileCarouselWindow.addEventListener(
+    "touchstart",
+    (event) => {
+      mobileDragging = true;
+      mobileDragStartX = event.touches[0].clientX;
+      mobileDragCurrentX = mobileDragStartX;
+      mobileCarouselWindow.classList.add("is-dragging");
+    },
+    { passive: true }
+  );
+
+  mobileCarouselWindow.addEventListener(
+    "touchmove",
+    (event) => {
+      if (!mobileDragging) {
+        return;
+      }
+
+      mobileDragCurrentX = event.touches[0].clientX;
+      const dragDistance = mobileDragCurrentX - mobileDragStartX;
+
+      setMobileDragOffset(dragDistance);
+    },
+    { passive: true }
+  );
+
+  mobileCarouselWindow.addEventListener("touchend", () => {
+    if (!mobileDragging) {
+      return;
+    }
+
+    const dragDistance = mobileDragCurrentX - mobileDragStartX;
+    const dragThreshold = 32;
+
+    mobileCarouselWindow.classList.remove("is-dragging");
+
+    if (dragDistance < -dragThreshold) {
+      moveMobileNav("left");
+      mobileDragging = false;
+      return;
+    }
+
+    if (dragDistance > dragThreshold) {
+      moveMobileNav("right");
+      mobileDragging = false;
+      return;
+    }
+
+    setMobileDragOffset(0);
+    mobileDragging = false;
+  });
+
+  mobileCarouselWindow.addEventListener(
+    "wheel",
+    (event) => {
+      if (Math.abs(event.deltaX) <= Math.abs(event.deltaY)) {
+        return;
+      }
+
+      event.preventDefault();
+
+      if (event.deltaX > 0) {
+        moveMobileNav("left");
+      }
+
+      if (event.deltaX < 0) {
+        moveMobileNav("right");
+      }
+    },
+    { passive: false }
+  );
+}
