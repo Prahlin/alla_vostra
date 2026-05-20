08029cf7dd72c3aeda7aa3c7dc616c3364adeb18
Prahlin <martin@prahlproductions.com>
2026-05-19 22:48:42 -0400

    Update shop page and set up mobile app structure

---
+++ mob/app/shop.js
@@ -0,0 +1,7 @@
+import ShippingPromo from "../components/ShippingPromo";
+
+import ProductCard from "../components/ProductCard";
+
+import { products } from "../data/products";
+
+import styles from "../styles/shopStyles";
